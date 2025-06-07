fb173c59a7ea9b54f3387033a0e1b424864c5475
Tereza Lunerova <lunerova.tereza@seznam.cz>
2025-06-07 13:15:52 +0200

    CalendarPage css

---
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -0,0 +1,16 @@
+import './style.css';
+import { Link } from 'react-router-dom';
+
+export const CalendarPage = () => {
+  return (
+    <div className="container">
+      <main>
+        <div className="page-wrapper">
+          <div className="block">
+            <div className="block-calendar-content"></div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
