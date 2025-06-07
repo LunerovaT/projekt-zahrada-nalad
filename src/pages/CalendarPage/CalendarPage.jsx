da82750daf4fe3f547fe2de443fe7ebaab10c643
Tereza Lunerova <lunerova.tereza@seznam.cz>
2025-06-07 14:37:02 +0200

    pagefield

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,14 +1,30 @@
 import './CalendarPage.css';
 import { Link } from 'react-router-dom';
 
+const days = ['pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek'];
+
+const App = () => (
+  <>
+    <h1>Pracovní dny</h1>
+    <ol className="days">
+      {days.map((day) => (
+        <li>{day}</li>
+      ))}
+    </ol>
+  </>
+);
+
 export const CalendarPage = () => {
+  const fields = Array.from({ length: 28 });
   return (
     <div className="container">
       <main>
         <div className="page-wrapper">
           <div className="block">
             <div className="block-calendar-content">
-              <p> zouška text ?? </p>
+              {fields.map((_, index) => (
+                <div key={index} className="calendar-field"></div>
+              ))}
             </div>
           </div>
         </div>
