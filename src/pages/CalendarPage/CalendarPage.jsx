19eb0e4822429f434acc64182ee2ee44ae6754b5
Tereza Lunerova <lunerova.tereza@seznam.cz>
2025-06-08 18:36:36 +0200

    calndar fix

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,6 +1,7 @@
 import { FlowerButton } from '../components/FlowerButton/FlowerButton';
 import './CalendarPage.css';
 import nextButton from './img/next-button.svg';
+import previousButton from './img/previous-button.svg';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
@@ -10,48 +11,61 @@ import {
   CalendarGrid,
   Heading,
 } from 'react-aria-components';
+import { getLocalTimeZone, now } from '@internationalized/date';
+
+const keyByDate = (date) => `${date.year}-${date.month}`;
+const loadForDate = (date) => {
+  const data = localStorage.getItem(keyByDate(date));
+  if (data === null) {
+    return Array.from({ length: 31 }).map(() => null);
+  }
+  console.log(data);
+  return JSON.parse(data);
+};
 
 export const CalendarPage = () => {
-  const thisMonthKey = '2025-06';
-  const daysInMonth = 30;
-
-  const [days, setDays] = useState(() => {
-    const data = localStorage.getItem(thisMonthKey);
-    if (data === null) {
-      return Array.from({ length: daysInMonth }).map(() => null);
-    }
-    console.log(data);
-    return JSON.parse(data);
-  });
+  const [date, setDate] = useState(() => now(getLocalTimeZone()));
+  const [days, setDays] = useState(() => loadForDate(date));
+  const chosenMonthKey = keyByDate(date);
 
   useEffect(() => {
-    localStorage.setItem(thisMonthKey, JSON.stringify(days));
-  }, [days, thisMonthKey]);
+    localStorage.setItem(chosenMonthKey, JSON.stringify(days));
+  }, [days, chosenMonthKey]);
 
   const options = ['happy', 'sad', 'calm', 'anxious', 'angry'];
   console.log(days);
 
   const navigate = useNavigate();
 
-  /*
-  if (data?.flowerId) {
-                                navigate(
-                                  `/flowerpage/${thisMonthKey}/${index}`,
-                                );
-                              }
-                                */
-
   return (
-    <Calendar aria-label="Kalendář květin">
+    <Calendar
+      aria-label="Kalendář květin"
+      onFocusChange={(date) => {
+        setDays(loadForDate(date));
+        setDate(date);
+      }}
+      onChange={(date) => {
+        const dayIndex = date.day - 1;
+        if (days[dayIndex]) {
+          navigate(`/flowerpage/${chosenMonthKey}/${dayIndex}`);
+        }
+      }}
+    >
       <div className="container">
         <main>
           <div className="page-wrapper">
             <div className="block block-flower-background">
               <div className="calendar-page-calendar">
                 <header>
-                  <Button slot="previous">◀</Button>
-                  <Heading />
-                  <Button slot="next">▶</Button>
+                  <div class="next">
+                    <Button slot="previous">
+                      <img src={previousButton} alt="Předchozí" />
+                    </Button>
+                    <Heading />
+                    <Button slot="next">
+                      <img src={nextButton} alt="Další" />
+                    </Button>
+                  </div>
                 </header>
                 <div className="block-calendar-content">
                   <CalendarGrid>
@@ -71,7 +85,6 @@ export const CalendarPage = () => {
                   </CalendarGrid>
                 </div>
               </div>
-
               <div className="calendar-page-options">
                 {options.map((flowerId, index) => (
                   <FlowerButton
